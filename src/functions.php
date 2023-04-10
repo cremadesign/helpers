5187622fed6bd99c37040d668423aadf0efa3ef3
Stephen Ginn <callaginn@users.noreply.github.com>
2023-04-10 10:26:18 -0500

    Added functions and classes from email framework

--- src/functions.php
+++ src/functions.php
@@ -3,18 +3,45 @@
 	putenv("HOME=$home");
 	putenv('PATH=$PATH:/bin:/usr/bin:/usr/local/bin:$HOME/bin');
 	
-	// =========================================================================
-
+	// Yaml Parser
+	use Symfony\Component\Yaml\Yaml;
+	use Symfony\Component\Yaml\Exception\ParseException;
+	
+	function settings($host=null) {
+		$host = isset($host) ? $host : $_SERVER["HTTP_HOST"];
+		$settings = (object) [
+			'env' => "live",
+			'twig' => []
+		];
+		
+		switch ($host) {
+			case contains(".test", $host):
+				$settings->env = "local";
+				$settings->twig = [
+					'debug' => true
+				];
+			break;
+			case contains("cremadesignstudio", $host):
+				$settings->env = "proof";
+			break;
+		}
+		
+		return $settings;
+	}
+	
+	
+	// == STRINGS & ARRAYS =====================================================
+	
 	// https://stackoverflow.com/a/34499778
 	function ordinal($number) {
 		return date("S", mktime(0, 0, 0, 0, $number, 0));
 	}
-
+	
 	// Returns a random array item
 	function random($array) {
 		return $array[rand(0, count($array) - 1)];
 	}
-
+	
 	function slugify($string) {
 		return strtolower(trim(preg_replace('/[^A-z0-9-]+/', '-', $string), '-'));
 	}
@@ -25,27 +52,27 @@
 	function contains($query, $string) {
 		return strpos($string, $query) !== false;
 	}
-
+	
 	// https://stackoverflow.com/a/55229702
 	class Haystack {
 		public $value;
-
+		
 		public function __construct($value) {
 			$this->value = $value;
 		}
-
+		
 		public function contains($needle) {
 			if (strpos($this->value, $needle) !== false) return $this;
 		}
 	}
-
+	
 	function findItem($obj, $query) {
 		return $obj[array_search($query, array_column($obj, 'id'))];
 	}
 	
 	
 	// == FILE RELATED =========================================================
-
+	
 	function removeDir($target) {
 		$directory = new RecursiveDirectoryIterator($target,  FilesystemIterator::SKIP_DOTS);
 		$files = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
@@ -58,15 +85,68 @@
 		}
 		rmdir($target);
 	}
-
+	
 	function removeDirs($targets) {
 		foreach ($targets as $dir) {
 			if (!is_dir($dir)) {
 				mkdir($dir, 0777, true);
 			}
-
+			
 			removeDir($dir);
 		}
+	}
+	
+	
+	// == LOAD DATA ============================================================
+	
+	function loadData($filename, $basedir = "data/") {
+		$ext = pathinfo($filename, PATHINFO_EXTENSION);
+		$contents = file_get_contents("$basedir$filename");
+		
+		if ($ext == 'yaml') {
+			$contents = str_replace("\t", '    ', $contents);
+			return Yaml::parse($contents);
+		} else {
+			return json_decode($contents, true);
+		}
+	}
+	
+	class FrontMatter {
+		public $data;
+		public $content;
+		
+		// Methods
+		function init($content) {
+			// Get position of second instance of "---"
+			$pos = strpos($content, '---', strpos($content, '---') + 1);
+			$this->yaml = Yaml::parse(substr($content, 0, $pos));
+			$this->content = substr($content, $pos+3);
+		}
+	}
+	
+	function jsonToCSV($data, $cfilename) {
+		$fp = fopen($cfilename, 'w');
+		$header = false;
+		
+		foreach ($data as $row) {
+			if (empty($header)) {
+				$header = array_keys($row);
+				fputcsv($fp, $header);
+				$header = array_flip($header);
+			}
+			
+			fputcsv($fp, array_merge($header, $row));
+		}
+		
+		fclose($fp);
+		
+		return;
+	}
+	
+	function XMLtoJSON($url) {
+		$xml_string = file_get_contents($url);
+		$xml = simplexml_load_string($xml_string, 'SimpleXMLElement', LIBXML_NOCDATA);
+		return json_decode(json_encode($xml), true);
 	}
 	
 	
@@ -82,22 +162,58 @@
 		if (contains('.test', $_SERVER["HTTP_HOST"])) {
 			$project_root = dirname(dirname($_SERVER["SCRIPT_FILENAME"]));
 			$logfile = "logs.log";
-	
+			
 			if (is_array($message)) {
 				$message = json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 				$logfile = "logs.json";
 			}
-	
+			
 			file_put_contents("$project_root/$logfile", $message . "\n");
+		}
+	}
+	
+	/*/
+		$console = new HtmlConsole();
+		$console->addMessage("Stephen");
+		$console->addMessage((array) $options);
+		$data['consoleString'] = $console->messages(true) ?? false;
+	/*/
+	
+	class HtmlConsole {
+		public function __construct() {
+			$this->messages = [];
+		}
+		
+		public function addMessage($value) {
+			$this->messages[] = $value;
+		}
+		
+		public function size() {
+			return count($this->messages);
+		}
+		
+		public function messages($stringify = null) {
+			if ($this->size() == 0) {
+				return false;
+			}
+			
+			if (isset($stringify)) {
+				$messageString = "";
+				
+				foreach ($this->messages as $message) {
+					$msg = json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+					$messageString .=  "console.log($msg);\n";
+				}
+				
+				return "<script>$messageString</script>";
+			} else {
+				return $this->messages;
+			}
 		}
 	}
 	
 	function console($message) {
 		echo "<script>console.log(" . json_encode($message) . ")</script>";
-	}
-	
-	function loadData($filename) {
-		return json_decode(file_get_contents("data/$filename"), true);
 	}
 	
 	function printJSON($data) {
