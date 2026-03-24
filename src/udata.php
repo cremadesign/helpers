3ae50128d0bc79dc50afd065b004f237f161a13b
callaginn <callaginn@users.noreply.github.com>
2026-03-24 12:44:34 -0500

    Updated UData class with stability fixes and pro API support
    - Added support for the ip-api.com paid plan. You can either pass an API key to unlock the pro endpoint or leave it out to use the free tier.
    - Removed the two unused backup location providers and the ability to switch between them via URL parameter.
    - Fixed several stability and security issues around how location data is fetched and how the class handles edge cases.

--- src/udata.php
+++ src/udata.php
@@ -2,41 +2,38 @@
 	use DeviceDetector\ClientHints;
 	use DeviceDetector\DeviceDetector;
 	use DeviceDetector\Parser\Client\Browser;
-	
+
 	// Get Location Data from IP address
 	class UData extends \stdClass {
-		public function __construct() {
+		public function __construct($apiKey = null) {
+			$this->apiKey = $apiKey;
 			$this->ip = $this->getIP();
 			$protocol = (isset($_SERVER['HTTPS']) and $_SERVER['HTTPS'] == "on") ? "https" : "http";
 			$this->referer = $_SERVER['HTTP_REFERER'] ?? "$protocol://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 			$this->userAgent = $_SERVER['HTTP_USER_AGENT'] ?? null;
-			
-			$urls = [
-				"http://ip-api.com/json/$this->ip",
-				"http://ipinfo.io/$this->ip/json",
-				"http://api.db-ip.com/v2/free/$this->ip"
-			];
-			
-			$version = $_GET['v'] ?? 0;
-			
-			$this->url = $urls[$version];
+
+			$this->url = $this->apiKey
+				? "https://pro.ip-api.com/json/{$this->ip}?key={$this->apiKey}"
+				: "http://ip-api.com/json/{$this->ip}";
 		}
-		
+
 		public function getIP() {
 			$ip = $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
-			
+
+			$ip = filter_var($ip, FILTER_VALIDATE_IP) ? $ip : $_SERVER['REMOTE_ADDR'];
+
 			if ($ip == '127.0.0.1') {
-				$ip = trim(`dig +short myip.opendns.com @resolver1.opendns.com` ?? '');
+				$ip = trim(file_get_contents('https://api.ipify.org') ?: '');
 			}
-			
+
 			return $ip;
 		}
-		
+
 		public function getData() {
 			if (is_null($this->userAgent)) {
 				return [];
 			}
-			
+
 			// Build and Return JSON Array
 			$data = [
 				'lang' => substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '??', 0, 2),
@@ -50,41 +47,49 @@
 				],
 				'referer' => "$this->referer"
 			];
-			
+
 			// Get Browser and OS from User Agent
 			$clientHints = ClientHints::factory($_SERVER);
 			$dd = new DeviceDetector($this->userAgent, $clientHints);
 			$dd->parse();
-			
+
 			if ($dd->isBot()) {
 				$botInfo = $dd->getBot();
 				$data['uagent']['browser'] = "{$botInfo['name']} ({$botInfo['category']})";
 			} else {
 				$clientInfo = $dd->getClient();
 				$osInfo = $dd->getOs();
-				
+
 				if ($clientInfo) {
 					$data['uagent']['browser'] = "{$clientInfo['name']} {$clientInfo['version']}";
 				}
-				
+
 				if ($osInfo) {
 					$data['uagent']['os'] = "{$osInfo['name']} {$osInfo['version']}";
 				}
 			}
-			
-			if ($location = `curl $this->url` ?? false) {
-				$location = json_decode($location, true);
-				$data['location'] = $location;
+
+			$ch = curl_init($this->url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+			$response = curl_exec($ch);
+			curl_close($ch);
+
+			if ($response) {
+				$location = json_decode($response, true);
+				if ($location) {
+					$data['location'] = $location;
+				}
 			}
-			
+
 			$this->data = $data;
-			
+
 			return $data;
 		}
-		
+
 		public function __toString() {
 			header('Content-Type: application/json');
-			return json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+			return json_encode($this->data ?? [], JSON_PRETTIER);
 		}
 	}
 ?>
